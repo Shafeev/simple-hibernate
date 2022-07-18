9e25d5b833f79360dfb76504719204e39727afd4
makhmud-shafeev <shafeevmkf@gmail.com>
2022-07-18 21:37:41 +0300

    fix file name sql

--- src/main/java/ru/mcs/simple/model/movie/Movie.java
+++ src/main/java/ru/mcs/simple/model/movie/Movie.java
@@ -67,7 +67,6 @@ public class Movie {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", yearOfProduction=" + yearOfProduction +
-                ", actors=" + actors +
                 '}';
     }
 }
